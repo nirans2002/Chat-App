552887b13b84c38b40e17dab6173c27cb2880ad1
Niran S Narayanan <nirans2002@gmail.com>
2021-08-26 22:36:58 +0530

    chatscreen 1

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:chatapp/components/filled_outline_button.dart';
 import 'package:chatapp/constants.dart';
+import 'package:chatapp/models/Chat.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -8,6 +9,8 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         Container(
+          padding: EdgeInsets.fromLTRB(
+              kDefaultPadding, 0, kDefaultPadding, kDefaultPadding),
           color: kPrimaryColor,
           child: Row(
             children: [
@@ -15,10 +18,46 @@ class Body extends StatelessWidget {
                 press: () {},
                 text: "Recent Messages",
               ),
+              SizedBox(width: kDefaultPadding),
+              FillOutlineButton(
+                press: () {},
+                text: "Active",
+                isFilled: false,
+              ),
             ],
           ),
         ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: chatsData.length,
+            itemBuilder: (context, index) => ChatCard(chat: chatsData[index]),
+          ),
+        ),
       ],
+    );
+  }
+}
+
+class ChatCard extends StatelessWidget {
+  const ChatCard({
+    Key key,
+    this.chat,
+  }) : super(key: key);
+
+  final Chat chat;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: kDefaultPadding, vertical: kDefaultPadding * 0.75),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 24,
+            backgroundImage: AssetImage(chat.image),
+          ),
+        ],
+      ),
     );
   }
 }
